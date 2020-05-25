5c7d67e6ebd7ce7f6c9ebfade035f5c251a4f383
Dmitry <leo2000@list.ru>
2020-05-26 00:10:43 +0300

    Update isg.h

--- kernel/isg.h
+++ kernel/isg.h
@@ -1,6 +1,11 @@
 #ifndef _ISG_H
 #define _ISG_H
 
+#include <linux/version.h>
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 38)
+#error Minimum Kernel version is 2.6.38
+#endif
+
 #define INIT_SESSION  0x01
 #define INIT_BY_SRC   0x02
 #define INIT_BY_DST   0x04
